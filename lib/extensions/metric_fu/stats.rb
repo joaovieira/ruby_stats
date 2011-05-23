5df35cd456d87b7f6a931975d864e321c4882c67
Joao Vieira <shocker@abandonados.info>
2011-05-23 02:02:48 +0100

    Alterado o html gerado para a informacao detalhada.

--- lib/extensions/metric_fu/stats.rb
+++ lib/extensions/metric_fu/stats.rb
@@ -2,20 +2,9 @@ require 'metric_fu'
 
 class MetricFu::Stats
 
-    def emit
+	def emit
 	  project_dir = File.join(MetricFu.base_directory, '..')
-      @output = `cd '#{project_dir}' && rake stats`
+      `cd #{project_dir} && rake stats > #{metric_directory + '/stats.txt'}`
     end
-
-    def analyze
-      lines = remove_noise(@output)
-
-      @stats = {}
-
-      set_global_stats(lines.pop)
-      set_granular_stats(lines)
-
-      @stats
-    end
-  
+	
 end
--- lib/ruby_stats/analyser.rb
+++ lib/ruby_stats/analyser.rb
@@ -9,8 +9,8 @@ module RubyStats
         config.base_directory = File.join(results_dir, 'metrics')
 	    config.data_directory = File.join(config.base_directory, '_data')
 		config.scratch_directory = File.join(config.base_directory, 'scratch')
-	    config.output_directory = File.join(config.base_directory, 'output', "#{date.strftime("%Y%m%d%H%M")}", RubyStats::ANALYSER.underscore)	
-  	    config.template_class = RstatsTemplate	
+	    config.output_directory = File.join(config.base_directory, 'output', "#{date.strftime("%Y%m%d%H%M%S")}", RubyStats::ANALYSER.underscore)
+		config.template_class = RstatsTemplate	
 	    config.metrics = [:stats]
   	    config.graphs = [:stats]
       end  
@@ -18,7 +18,7 @@ module RubyStats
       # Run metric fu!
       MetricFu.metrics.each {|metric| MetricFu.report.add(metric) }
       MetricFu.report.save_output(MetricFu.report.to_yaml, MetricFu.base_directory, "report.yml")
-      MetricFu.report.save_output(MetricFu.report.to_yaml, MetricFu.data_directory, "#{date.strftime("%Y%m%d%H%M")}.yml")
+      MetricFu.report.save_output(MetricFu.report.to_yaml, MetricFu.data_directory, "#{date.strftime("%Y%m%d%H%M%S")}.yml")
 
       MetricFu.graphs.each {|graph| MetricFu.graph.add(graph, MetricFu.graph_engine) }
       MetricFu.graph.generate
